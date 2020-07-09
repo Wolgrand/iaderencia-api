d8cc52b4273382a1fedec1d7b844f2d07c1d378d
Wolgrand Neto <wolgrand@live.com>
2020-07-09 11:14:18 -0300

    Fix ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+module.exports = {
+  name: 'default',
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASS,
+  database: process.env.POSTGRES_DB,
+  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
+  migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/shared/infra/typeorm/migrations',
+  },
+};
