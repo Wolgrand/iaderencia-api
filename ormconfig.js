1c8d42c1a732ee14bc2a3610e2657588fc53bdc8
Wolgrand Neto <wolgrand@live.com>
2020-07-09 11:30:05 -0300

    Fix Paths

--- ormconfig.js
+++ ormconfig.js
@@ -8,9 +8,9 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASS,
   database: process.env.POSTGRES_DB,
-  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
-  migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+  entities: ['./dist/modules/**/infra/typeorm/entities/*.js'],
+  migrations: ['./dist/shared/infra/typeorm/migrations/*.js'],
   cli: {
-    migrationsDir: './src/shared/infra/typeorm/migrations',
+    migrationsDir: './dist/shared/infra/typeorm/migrations',
   },
 };
